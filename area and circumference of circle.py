d4b901bdd8d2626419e539555e1b7cdd0a081d78
satyam <satyamparmarsatyam@gmail.com>
2025-08-25 21:52:23 +0530

    area and circumference program

---
+++ area and circumference of circle.py
@@ -0,0 +1,5 @@
+pi = 3.14
+radius = int(input("enter radius : "))
+area = pi*radius**2
+cirucmference = 2*pi*radius
+print(f"the area of circle is {area} \n the circumference of circle is {cirucmference} ")
