a89113e3fcde32554fc8caf72c067fa2c8ad070c
satyam <satyamparmarsatyam@gmail.com>
2025-09-04 20:47:36 +0530

    Added OOP.py with examples of classes, inheritance, and polymorphism

---
+++ OOP.py
@@ -0,0 +1,63 @@
+# the main concept of oop are class object inheritance polymorphism abstraction incapsulation
+#class is basic template for creating objects in python we use def keyword for class
+# syntax class employee:
+# objects are run time entities
+# obj=myclass() is for creating objects
+# inheritance is for code reuseability
+# abstraction means only shoe essential information ton user (_ for abstraction)
+
+#code 1
+class programmer:
+    company="microsoft"
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+    def get_info(self):
+        print(f"the {self.name} is working in {self.company} and his salary is {self.salary}")
+satyam=programmer("satyam",90000)
+diksha=programmer("diksha",90000)
+satyam.get_info()
+diksha.get_info()
+#code 2
+
+class square:
+    def __init__(self,num):
+        self.num=num
+    def squaree(self):
+        return self.num*self.num
+sqr=square(8)
+print(sqr.squaree())
+
+@staticmethod
+def greet():
+    print("good to see you")
+
+# inheritance 
+class Animal:
+    def __init__(self,name,legs,horns):
+        self.name=name
+        self.legs = legs
+        self.horns=horns
+
+class Pets(Animal):
+    def pets(self):
+        print(self.name,self.legs,self.horns)
+
+class Dog(Pets):
+    @classmethod
+    def bark(cls):
+        print("dog is barking")
+        
+# example showing use of super()
+class Mammal(Dog):
+    def __init__(self, name, legs, horns, has_fur=True):
+        super().__init__(name, legs, horns)  # calls Animal.__init__ via the MRO
+        self.has_fur = has_fur
+
+    def show(self):
+        print(f"{self.name}: legs={self.legs}, horns={self.horns}, has_fur={self.has_fur}")
+
+mamal = Mammal("Lion", 4, 0)
+mamal.show()
+mamal.pets()
+mamal.bark()
